d634637dd170021157fb8befe538626ec217fb49
brightonchebor <cheborbrighton805@gmail.com>
2025-01-25 14:48:06 +0300

    add apps

--- GIS/settings.py
+++ GIS/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'myapp',
+    "django.contrib.gis",
+    "rest_framework_gis",
 ]
 
 MIDDLEWARE = [
