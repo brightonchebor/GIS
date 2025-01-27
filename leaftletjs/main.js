fa0203a8072e5a2b4fd610d241993c3a5a241758
brightonchebor <cheborbrighton805@gmail.com>
2025-01-27 15:11:31 +0300

    cn

--- leaftletjs/main.js
+++ leaftletjs/main.js
@@ -11,9 +11,17 @@ function init() {
     const mymapp = L.map('mapid', {
         center: [48, 14],
         zoom: 4,
+        minZoom: 4,
+        zoomSnap: 0.25,
+        zoomDelta: 0.25,
+        easeLinearity: 0.5,
         layers : [
             openStreetMapStandard
         ]
+    })
+    
+    mymapp.on('zoom', function(e){
+        console.log(e.target._zoom)
     })
 
     const baseLayers = {
@@ -27,4 +35,7 @@ function init() {
     mymapp.on('click', function(event){
         console.log(event.latlng)
     })
+
+    // Marker
+    const pathMarker = L.marker({lat: 48.545705491847464, lng: 12.656250000000002}).addTo(mymapp)
 }
