37b447fe6155b42830f1ba17e10d941c9741187a
brightonchebor <cheborbrighton805@gmail.com>
2025-01-27 14:08:30 +0300

    add location

--- leaftletjs/main.js
+++ leaftletjs/main.js
@@ -1,13 +1,30 @@
 window.onload = init;
 
 function init() {
-    const myapp = L.map('mapid', {
+
+    const mapElement = document.getElementById('mapid')
+
+    const openStreetMapStandard = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
+    )
+
+    const mymapp = L.map('mapid', {
         center: [48, 14],
         zoom: 4,
         layers : [
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
-            )
+            openStreetMapStandard
         ]
+    })
+
+    const baseLayers = {
+        'openStreetMapStandard':openStreetMapStandard,
+    }
+
+    const layerControls = L.control.layers(
+        baseLayers, {}, {}
+    ).addTo(mymapp)
+
+    mymapp.on('click', function(event){
+        console.log(event.latlng)
     })
 }
