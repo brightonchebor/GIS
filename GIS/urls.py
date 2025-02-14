482462ccd5c483e24b97fb51f74de96095222a0e
brightonchebor <cheborbrighton805@gmail.com>
2025-02-14 16:46:51 +0300

    cn

--- GIS/urls.py
+++ GIS/urls.py
@@ -1,10 +1,25 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
     path('', include('facilities.urls')),
     # path('', include('frontend.urls')),
+
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/schema/redoc/", SpectacularRedocView.as_view(), name="redoc"),
+    path(
+        "api/v1/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(),
+        name="swagger-ui",
+    ),
 ]
+
